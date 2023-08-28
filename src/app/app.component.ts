b13ab0e8349ba013fbfded4c8d2c4fadddb87fe8
逸鴻 張 <winterer32@gmail.com>
2023-08-29 01:14:37 +0800

    串接API後端

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ITodo, TodoStatusType } from './models/todo.model';
 
 @Component({
@@ -6,7 +7,26 @@ import { ITodo, TodoStatusType } from './models/todo.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  todoDataList: ITodo[] = [];
+  ngOnInit(): void {
+    this.http.get<any>('https://localhost:7271/api/TodoList').subscribe(response => {
+      if (response.isSuccess) {
+        this.todoDataList = response.data.map((item: any) => {
+          return {
+            Status: item.status,
+            Context: item.context,
+            Editing: item.editing
+          };
+        });
+      } else {
+        console.error(response.errorMessage);
+      }
+    })
+  }
+
   title = 'ToDoList';
   placeholder = 'what needs to be done today??';
   attributeBinding = 'attributeBindingTest';
@@ -17,73 +37,14 @@ export class AppComponent {
   nowTodoStatusType = TodoStatusType.All;
   TodoStatusType = TodoStatusType;
 
-  // 使用ngFor精簡程式碼
-  // check1 = false;
-  // check2 = false;
-  // class繫結-精簡程式碼
-  // classCompleted1 = '';
-  // classCompleted2 = '';
-
-  // class的寫法
-  // todoDataList: Todo[] = ([
-  //   {
-  //     Status: true,
-  //     Context: '第一件事情',
-  //   },
-  //   {
-  //     Status: false,
-  //     Context: '第二件事情',
-  //   },
-  //   {
-  //     Status: false,
-  //     Context: '第三件事情',
-  //   },
-  //   // map()是RxJS語法，有空可以研究一下
-  // ]).map(data => new Todo(data.Status, data.Context));
-
-  // Interface的寫法
-  todoDataList: ITodo[] = ([
-    {
-      Status: true,
-      Context: '第一件事情',
-      Editing: false
-    },
-    {
-      Status: false,
-      Context: '第二件事情',
-      Editing: false
-    },
-    {
-      Status: false,
-      Context: '第三件事情',
-      Editing: false
-    },
-  ]);
-
   toggleAll() {
     this.toggleAllBtn = !this.toggleAllBtn;
     this.todoDataList.forEach((data) => {
       data.Status = this.toggleAllBtn;
     });
-
-    // 使用ngFor精簡程式碼
-    // this.check1 = this.toggleAllBtn;
-    // this.check2 = this.toggleAllBtn;
-    // class繫結-精簡程式碼
-    // if (this.toggleAllBtn) {
-    //   this.classCompleted1 = 'completed smallText';
-    //   this.classCompleted2 = 'completed smallText';
-    // } else {
-    //   this.classCompleted1 = '';
-    //   this.classCompleted2 = '';
-    // }
   }
 
   clickCheck(item: ITodo) {
-    // class的寫法
-    // item.toggle();
-
-
     // Interface的寫法
     item.Status = !item.Status;
 
@@ -94,40 +55,10 @@ export class AppComponent {
     }
   }
 
-  // 使用ngFor精簡程式碼
-  // clickCheck1() {
-  //   this.check1 = !this.check1;
-
-  //   // class繫結-精簡程式碼
-  //   // if (this.check1) {
-  //   //   this.classCompleted1 = 'completed smallText';
-  //   // } else {
-  //   //   this.classCompleted1 = '';
-  //   // }
-  // }
-
-  // 使用ngFor精簡程式碼
-  // clickCheck2() {
-  //   this.check2 = !this.check2;
-
-  //   // class繫結-精簡程式碼
-  //   // if (this.check2) {
-  //   //   this.classCompleted2 = 'completed smallText';
-  //   // } else {
-  //   //   this.classCompleted2 = '';
-  //   // }
-  // }
-
   delete(todo: ITodo) {
     // splice(index: 第幾個位置, deleteCount: 刪除幾個物件)
     this.todoDataList = this.todoDataList.filter(data => data !== todo);
   }
-
-  // class的寫法
-  // add(input: HTMLInputElement) {
-  //   this.todoDataList.push(new Todo(false, input.value));
-  //   input.value = '';
-  // }
 
   // Interface的寫法
   add(input: HTMLInputElement) {
