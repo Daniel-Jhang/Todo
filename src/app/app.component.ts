4965b5c4405dc185ddfcfd40e212bdaefe0a8a36
Ethan Chang (張逸鴻) <ethanchang@cybersoft.com.tw>
2023-09-15 16:08:54 +0800

    利用HttpClient更新資料至後端伺服器

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,56 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  // 建構式
   constructor(private http: HttpClient, private toastr: ToastrService) {}
 
+  // 宣告變數
+  title = 'ToDoList';
+  placeholder = 'what needs to be done today??';
+  attributeBinding = 'attributeBindingTest';
+  toggleAllBtn = false;  // 事件繫結-元素click事件呼叫ts的函式
+  nowTodoStatusType = TodoStatusType.All;
+  TodoStatusType = TodoStatusType;
   todoDataList: ITodo[] = [];
+
   ngOnInit(): void {
+    this.getData();
+  }
+
+  add(input: HTMLInputElement) {
+    const newTodoContext: ITodo = {
+      TodoId: null,
+      Status: false,
+      Context: input.value,
+      Editing: false,
+    };
+    this.http
+      .post<any>('/api/TodoList', newTodoContext)
+      .subscribe((response) => {
+        if (response.isSuccess) {
+          this.todoDataList.push({
+            TodoId: response.data.todoId,
+            Status: response.data.status,
+            Context: response.data.context,
+            Editing: response.data.editing,
+          });
+          // 顯示新增成功的 toast message
+          this.toastr.success('新增成功', 'Success', {
+            timeOut: 3000, // 可自訂顯示時間
+          });
+          console.log(response.isSuccess);
+          console.log(this.todoDataList);
+        } else {
+          // 顯示新增失敗的 toast message
+          this.toastr.error('新增失敗', 'Error', {
+            timeOut: 3000, // 可自訂顯示時間
+          });
+        }
+      });
+    input.value = '';
+  }
+
+  getData(){
     this.http.get<any>('/api/TodoList').subscribe((response) => {
       if (response.isSuccess) {
         this.todoDataList = response.data.map((item: any) => {
@@ -38,15 +84,28 @@ export class AppComponent implements OnInit {
     });
   }
 
-  title = 'ToDoList';
-  placeholder = 'what needs to be done today??';
-  attributeBinding = 'attributeBindingTest';
+  update(item: ITodo) {
+    this.http.put<any>('/api/TodoList', item).subscribe(
+      (response) => {
+        item.Editing = false;
+        // 顯示更新成功的 toast message
+        this.toastr.success('更新成功', 'Success', {
+          timeOut: 3000, // 可自訂顯示時間
+        });
+      },
+      (error) => {
+        // 顯示更新失敗的 toast message
+        this.toastr.error('更新失敗', 'Error', {
+          timeOut: 3000, // 可自訂顯示時間
+        });
+      }
+    );
+  }
 
-  // 事件繫結-元素click事件呼叫ts的函式
-  toggleAllBtn = false;
-
-  nowTodoStatusType = TodoStatusType.All;
-  TodoStatusType = TodoStatusType;
+  delete(todo: ITodo) {
+    // splice(index: 第幾個位置, deleteCount: 刪除幾個物件)
+    this.todoDataList = this.todoDataList.filter((data) => data !== todo);
+  }
 
   toggleAll() {
     this.toggleAllBtn = !this.toggleAllBtn;
@@ -66,69 +125,21 @@ export class AppComponent implements OnInit {
     }
   }
 
-  delete(todo: ITodo) {
-    // splice(index: 第幾個位置, deleteCount: 刪除幾個物件)
-    this.todoDataList = this.todoDataList.filter((data) => data !== todo);
-  }
-
-  // Interface的寫法
-  add(input: HTMLInputElement) {
-    const newTodoContext: ITodo = {
-      TodoId: null,
-      Status: false,
-      Context: input.value,
-      Editing: false,
-    };
-    this.http
-      .post<any>('/api/TodoList', newTodoContext)
-      .subscribe((response) => {
-        if (response.isSuccess) {
-          this.todoDataList.push({
-            TodoId: response.data.todoId,
-            Status: response.data.status,
-            Context: response.data.context,
-            Editing: response.data.editing,
-          });
-          // 顯示新增成功的 toast message
-          this.toastr.success('新增成功', 'Success', {
-            timeOut: 3000, // 可自訂顯示時間
-          });
-          console.log(response.isSuccess);
-          console.log(this.todoDataList);
-        }else{
-           // 顯示新增失敗的 toast message
-           this.toastr.error('新增失敗', 'Error', {
-            timeOut: 3000, // 可自訂顯示時間
-          });
-        }
-      });
-    input.value = '';
-  }
-
   edit(item: ITodo) {
     item.Editing = true;
   }
 
-  update(item: ITodo) {
-    this.http.put('/api/TodoList', item).subscribe(
-      (response) => {
-        item.Editing = false;
-        // 顯示更新成功的 toast message
-        this.toastr.success('更新成功', 'Success', {
-          timeOut: 3000, // 可自訂顯示時間
-        });
-      },
-      (error) => {
-        // 顯示更新失敗的 toast message
-        this.toastr.error('更新失敗', 'Error', {
-          timeOut: 3000, // 可自訂顯示時間
-        });
-      }
-    );
-  }
-
   setTodoStatusType(type: number) {
     this.nowTodoStatusType = type;
+  }
+
+  ClearCompleted() {
+    this.todoDataList = this.todoActive;
+  }
+
+  btnFun(event: MouseEvent) {
+    alert('123');
+    console.log(event);
   }
 
   get nowTodoList(): ITodo[] {
@@ -156,14 +167,5 @@ export class AppComponent implements OnInit {
   // 取得已完成的 todo list
   get todoCompleted(): ITodo[] {
     return this, this.todoDataList.filter((data) => data.Status == true);
-  }
-
-  ClearCompleted() {
-    this.todoDataList = this.todoActive;
-  }
-
-  btnFun(event: MouseEvent) {
-    alert('123');
-    console.log(event);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 需要加入動畫模組
-import { ToastrModule } from 'ngx-toastr'; // 引入 ngx-toastr
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr'; // 引入 ngx-toastr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // 需要加入動畫模組
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule, // 記得加入 BrowserAnimationsModule
     ToastrModule.forRoot(), // 初始化 ngx-toastr 模組
